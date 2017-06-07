e094ae8117a74d1bdf3cd954d5f6b759c1227027
Stuart Anderson <emblem@gmail.com>
2017-06-07 14:07:25 -0400

    Find client auth in local dir

--- database.py
+++ database.py
@@ -6,7 +6,7 @@ class UserDatabase:
     def __init__(self):
         # use creds to create a client to interact with the Google Drive API
         scope = ['https://spreadsheets.google.com/feeds']
-        creds = ServiceAccountCredentials.from_json_keyfile_name('../client_secret.json', scope)
+        creds = ServiceAccountCredentials.from_json_keyfile_name('client_secret.json', scope)
         client = gspread.authorize(creds)
  
         # Find a workbook by name and open the first sheet
